8fa3e8dcc3090644db1735d07ffb76b09217395c
Seikened <fernanlee2131@gmail.com>
2021-10-29 23:20:48 -0500

    a

--- test/e2e-test.test.js
+++ test/e2e-test.test.js
@@ -5,3 +5,15 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 const app = require('../app').app;
+
+
+describe('Suit de prueba  e2e para el curso', () => {
+    it('Deberia devolver hola mundo', (done) => {
+        chai.request(app) //chai queremos que utilices nuestro servidor de nuestros ficheros
+            .get('/') //hasme esta llamada a este servidor
+            .end((err, res) => {     //Permite recojer el resultado de esta llamda
+                chai.assert.equal(res.text, 'Hello World!')  //S
+                done();
+            });
+    })
+});
